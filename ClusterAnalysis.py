38a9a7c11a149ee0fb5a9d56ad2f6e0c53175985
Nelavenit <valbor2002@gmail.com>
2021-05-17 16:18:29 +0300

    Added ClusterAnalysis.py with both Global and Average clustering coefficient computing

---
+++ ClusterAnalysis.py
@@ -0,0 +1,26 @@
+import networkx as nx
+
+
+def global_clustering_coefficient(g):
+    number_of_closed_triplets = 0
+    number_of_any_triplets = 0
+    num_of_nodes = len(g.nodes)
+
+    # computing number_of_any_triplets
+    for i in range(num_of_nodes):
+        num_of_neighbors = len(list(g.neighbors(i)))
+        number_of_any_triplets += num_of_neighbors * (num_of_neighbors - 1)
+
+    # computing number_of_closed_triplets
+    for i in range(num_of_nodes):
+        for j in g.neighbors(i):
+            for k in g.neighbors(j):
+                if i % 5 == 0 and k == 3011:
+                    print(i)
+                if k != i and g.has_edge(i, k):
+                    number_of_closed_triplets += 1
+    return number_of_closed_triplets / number_of_any_triplets
+
+
+def average_clustering_coefficient(g):
+    return nx.algorithms.cluster.average_clustering(g)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import InputAndProjection
 import ComponentsAnalysis
+import ClusterAnalysis
 
 
 G = InputAndProjection.input_and_project_whole_graph()
@@ -8,7 +9,12 @@ G = InputAndProjection.input_and_project_whole_graph()
 
 # print(G[2][1]["weight"])
 
+# isolated_tags = []
 # ls = ComponentsAnalysis.sizes_of_components(G, 2)
 # for i in range(len(ls)):
 #     if ls[i] != 0:
-#         print(i + 1)
+#         isolated_tags.append(i + 1)
+# print(isolated_tags)
+
+# print(ClusterAnalysis.global_clustering_coefficient(G))
+# print(ClusterAnalysis.average_clustering_coefficient(G))
