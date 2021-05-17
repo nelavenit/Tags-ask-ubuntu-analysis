0fa62b4b5c54d960147c7b9d2b360580d81d64f9
Nelavenit <valbor2002@gmail.com>
2021-05-17 19:33:12 +0300

    Fixed graphic plotting max_edge_degree = 1

--- GraphicsOutput.py
+++ GraphicsOutput.py
@@ -8,10 +8,11 @@ import ClusterAnalysis
 
 def number_of_components_maturation():
     ls = []
-    for i in range(1, config.maximum_edge_degree + 1):
+    for i in range(2, config.maximum_edge_degree + 1):
         g = InputAndProjection.input_and_project_graph(i)
         ls.append(len(ComponentsAnalysis.sizes_of_components(g)))
-    plt.plot(range(config.maximum_edge_degree), ls, 'o--r')
+
+    plt.plot(range(2, config.maximum_edge_degree + 1), ls, 'o--r')
     plt.xlabel("Maximum edge degree")
     plt.ylabel("Number of components")
     plt.title("Number of components maturation")
@@ -20,14 +21,14 @@ def number_of_components_maturation():
 
 def largest_component_size_maturation():
     ls = []
-    for max_deg in range(1, config.maximum_edge_degree + 1):
+    for max_deg in range(2, config.maximum_edge_degree + 1):
         g = InputAndProjection.input_and_project_graph(max_deg)
         sizes = ComponentsAnalysis.sizes_of_components(g)
-        ls.append(min(sizes) / config.total_number_of_nodes)
-
-    plt.plot(range(config.maximum_edge_degree), ls, 'o--r')
+        ls.append(max(sizes) / config.total_number_of_nodes)
+        print(max(sizes))
+    plt.plot(range(2, config.maximum_edge_degree + 1), ls, 'o--r')
     plt.xlabel("Maximum edge degree")
     plt.ylabel("Relative size of largest component")
-    plt.title("Size of largest maturation")
+    plt.title("Size of largest component maturation")
     plt.show()
 
