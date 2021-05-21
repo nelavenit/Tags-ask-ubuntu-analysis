65ac71538ae716d913387aa16c5a3369dcf54c91
Nelavenit <valbor2002@gmail.com>
2021-05-21 14:41:18 +0300

    margin changes

--- PlotOutput.py
+++ PlotOutput.py
@@ -3,7 +3,8 @@ import config as cfg
 
 
 # Scale is either "auto scale" or "from 0 to 1"
-def maturation_plot(values_list, plot_title, x_axis_label, y_axis_label, scale="auto-scale", margin=0.1):
+def maturation_plot(values_list, plot_title, y_axis_label, x_axis_label="Maximum edge degree",
+                    scale="auto-scale", margin=cfg.default_plot_margin):
     plt.plot(range(2, cfg.maximum_edge_degree + 1), values_list, 'o--r')
     if scale == "from zero to one":
         plt.axis([2 - margin, cfg.maximum_edge_degree + margin,
